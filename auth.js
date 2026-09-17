fde05f99b36e05f6d03ac440a9742a1d68d97979
khang20067 <phuminhkhang20067@gmail.com>
2026-09-17 19:16:29 +0700

    Implement login function in auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+function login(username, password) {
+  if (!username || !password) return false;
+  if (username === 'admin' && password === '123') return true;
+  return false;
+}
+module.exports = { login };
